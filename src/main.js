3a934624b5f43daf69f46f8f72b03777e6f63f93
AxlvesDev <vinicius.a2@hotmail.com>
2025-06-05 22:27:44 -0300

    Instalação de pacotes ok

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict"
+
+
+// importação da configuração do dayjs
+import "./libs/dayjs.js"
+
+// css
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
+
+import dayjs from "dayjs"
+console.log(dayjs().format("HH:mm"))
