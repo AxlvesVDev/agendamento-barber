916a37bc51ace62cafa422729e91deda74ad62e8
AxlvesDev <vinicius.a2@hotmail.com>
2025-06-17 21:55:11 -0300

    Iniciando integração com a API

---
+++ src/services/schedule-new.js
@@ -0,0 +1,18 @@
+import { apiConfig  } from "./api-config.JS";
+
+export async function scheduleNew({ id, name, when}) { 
+    try {
+        await fetch (`${apiConfig.baseURL}/schedules`, {
+            method: 'POST',
+            headers: { 
+                "Content-type":"application/json"
+            },
+            body: JSON.stringify( { id, name, when} ),
+        })
+
+        alert("Agendamento realizado com sucesso!")
+    } catch (error) {
+        console.log(error)
+        alert("Não foi possível agendar")
+    }
+}
